f0c8ccb7faac2b98c24cb3358d77cb2a001ca436
nashr <muhammad.nassirudin@gmail.com>
2014-12-04 10:25:32 +0700

    init classes

---
+++ src/HierarchicalCluster.java
@@ -0,0 +1,12 @@
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
+public class HierarchicalCluster extends Classifier {
+
+  @Override
+  public void buildClassifier(Instances arg0) throws Exception {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ src/PartitionalCluster.java
@@ -0,0 +1,13 @@
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
+
+public class PartitionalCluster extends Classifier {
+
+  @Override
+  public void buildClassifier(Instances arg0) throws Exception {
+    // TODO Auto-generated method stub
+
+  }
+
+}
